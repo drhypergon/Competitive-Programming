a07ddea07d395481e511230978a9c813be97d391
stanley.zhao <stanley.zhao@servicenow.com>
2018-07-31 16:41:50 -0700

    first commit

---
+++ 2Sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        int index1 = 0;
+        int index2 = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i+1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    vector<int> result;
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+            }
+        }
+        vector<int> result;
+        return result;
+    }
+};
