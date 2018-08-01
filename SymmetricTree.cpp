dc55547cd450ef167a10f57464ac1ad4ebb728ad
drhypergon <41973653+drhypergon@users.noreply.github.com>
2018-07-31 17:05:44 -0700

    Create SymmetricTree.cpp

---
+++ SymmetricTree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) 
+    {
+        return isMirror(root, root);
+    }
+    
+    bool isMirror(TreeNode *root1, TreeNode *root2)
+    {
+        if (root1 == NULL && root2 == NULL)
+        {
+            return true;
+        }
+        if (root1 == NULL && root2 != NULL)
+        {
+            return false;
+        }
+        if (root1 != NULL && root2 == NULL)
+        {
+            return false;
+        }
+        if (root1->val == root2->val)
+        {
+            if ((isMirror(root1->left, root2->right) && (isMirror(root1->right, root2->left))))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
