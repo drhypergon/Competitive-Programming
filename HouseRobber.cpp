f3cc88929a5f38a3b7e1b9a591f399946d48d443
drhypergon <41973653+drhypergon@users.noreply.github.com>
2018-07-31 17:21:48 -0700

    Create HouseRobber.cpp

---
+++ HouseRobber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) 
+    {
+        if (nums.size() == 0)
+        {
+            return 0;
+        }
+        vector<int> result(nums.size());
+        result[0] = nums[0];
+        result[1] = max(nums[0], nums[1]);
+        
+        for (int i = 2; i < nums.size(); i++)
+        {
+            result[i] = max(result[i-1], (result[i-2] + nums[i]));
+        }
+        return result[nums.size()-1];
+    }
+};
