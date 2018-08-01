f38c653905b62d8095bcd575d7c26a9f8454fb4f
drhypergon <41973653+drhypergon@users.noreply.github.com>
2018-07-31 18:28:56 -0700

    Create SubTree.cpp

---
+++ SubTree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* s, TreeNode* t) 
+    {
+        if (t == NULL)
+        {
+            return true;
+        }
+        if (s == NULL)
+        {
+            return false;
+        }
+        if (areIdentical(s, t))
+        {
+            return true;
+        }
+        return isSubtree(s->left, t) || isSubtree(s->right, t);
+    }
+    
+    bool areIdentical(TreeNode *s, TreeNode *t)
+    {
+        if (s == NULL && t == NULL)
+        {
+            return true;
+        }
+        if (s == NULL || t == NULL)
+        {
+            return false;
+        }
+        return ((s->val == t->val) && areIdentical(s->left, t->left) && areIdentical(s->right, t->right));
+    }
+};
